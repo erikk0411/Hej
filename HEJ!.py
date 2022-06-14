12b91539ff287a495675af025deb8214ed43a65b
erikk0411 <107497306+erikk0411@users.noreply.github.com>
2022-06-14 18:49:40 +0200

    HEJ

---
+++ HEJ!.py
@@ -0,0 +1,9 @@
+
+print("skriv heltal viderfan")
+while True:
+    t=int(input())
+    j=int(input())
+    print ("You cheated dumma FAN,",)
+    print ("HAHAHAHAHAHAHA DUMMA FAN!!!!!!")
+    print ("Had you won, the prize was a golden ruby laser!!")
+    print (t**3 + j**3)
